93f82d3b74cf69a48bfddf5a8ffd16e0317792d8
Neon Shadow YT <alexplay656@gmail.com>
2023-01-30 22:15:37 +0700

    Revert "Update and rename Script/Monetization/Monetizationtest.cs to Скрипт/Монетизация/Monetizationtest.cs"
    This reverts commit 628567ed0c9a563e3b4f29ae0ab427a59063a0cb.

--- Script/Monetization/Monetizationtest.cs
+++ Script/Monetization/Monetizationtest.cs
@@ -5,6 +5,7 @@ public class Monetizationtest : MonoBehaviour
 {
     public void Start()
     {
-        
+        //if (Advertisement.isSupported) Advertisement.Initialize("4722287", false);
+        //Advertisement.Show("video");
     }
 }

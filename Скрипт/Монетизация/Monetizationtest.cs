628567ed0c9a563e3b4f29ae0ab427a59063a0cb
Neon Shadow YT <97021451+NeonShadowYT@users.noreply.github.com>
2023-01-30 22:14:25 +0700

    Update and rename Script/Monetization/Monetizationtest.cs to Скрипт/Монетизация/Monetizationtest.cs

--- Скрипт/Монетизация/Monetizationtest.cs
+++ Скрипт/Монетизация/Monetizationtest.cs
@@ -5,7 +5,6 @@ public class Monetizationtest : MonoBehaviour
 {
     public void Start()
     {
-        //if (Advertisement.isSupported) Advertisement.Initialize("4722287", false);
-        //Advertisement.Show("video");
+        
     }
 }
